2b05f231d86a7fdd5fe657f1fefb157ec1a25c72
eumontanhabjj <denilsonjunnior98@gmail.com>
2024-07-08 19:18:18 -0300

    feat/ estrutura de repetição while

---
+++ AULA_6/atividade_aula_6_while.js
@@ -0,0 +1,12 @@
+// 1 - Obrigatoriamente utilizando o laço de repetição "while" faça com que um valor chamado "Contador" vá de 0 até 10, imprimindo cada vez que o valor incrementar.
+// 2 - Crie uma variável chamada contador e atribua o valor 0;
+// 3 - Escreva um WHILE
+// Obs: A tradução de WHILE significa ENQUANTO.
+// 4 - Vamos utilizar um operador "<" que é MENOR QUE.
+
+let contador = 0;
+while (contador <= 10) {
+  console.log("Valor do contador:", contador);
+  console.log("Ainda é menor que 10:");
+  contador++;
+}
---
+++ AULA_6/estrutura_de_repetição.js
@@ -0,0 +1,10 @@
+// O QUE É UMA ESTRUTURA DE REPETIÇÃO?
+// QUAL SUA UTILIDADE?
+
+let contador = 0
+
+while (contador < 5) {
+    console.log("Valor do Contador:", contador);
+    console.log("Ainda é menor que 5!");
+    contador++;
+}
