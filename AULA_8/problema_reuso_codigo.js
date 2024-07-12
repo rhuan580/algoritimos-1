6b3e91bf3d8e1b43e63e63df7de54aa6938bfad0
eumontanhabjj <denilsonjunnior98@gmail.com>
2024-07-12 19:51:43 -0300

    feat: calculo

---
+++ AULA_8/problema_reuso_codigo.js
@@ -0,0 +1,36 @@
+// 1 - Uma rede educacional chamada "Big EDUCATION" que é dona
+// de varios projetos na area de educaçao tem um novo projeto.
+// Essa rede de educação tem uma unidade que apenas atende
+// pessoas com dificuldade de atividade devido transtornos.
+// Nessa mesma  unidade eles devem ensinar matemarica de forma
+// diferente para que os alunos consigam envoluir melhor nas atividades.
+
+// é necessario um ensino diferente da area de exatas para melhor
+// absorção do conteúdo.
+// Dessa forma a rede decidiu encomendar um software "Sob demanda"
+// Sendo o MVP de uma ferramenta que ensina como formas geometricas
+// funcionam (o programa exibe elas em 3D e também permite interações)
+
+// Profissional de produtos
+// Product Manager (Gestor do Produito - Fala co o cliente)
+// Product Owner (Dono do produto - Passa os requisitos para a equipe)
+
+// 1 - Passa em algoritmo a formula para calcular a area de um circulo
+//
+// 1 -:. Criar uma constante chamada pi e atribuir o valor 3.14
+// 2:. Criar uma constante chamada raio e atribuir o valor 5
+// 3:. Criar uma constante area e atribuir pi * (raio * raio)
+
+// 2 - De ultima hora conversei com o cliente e descobrir
+// que necessito reutilizar essa formula em outros trechos
+// do programa/sofware.
+
+// FUNÇÃO
+
+function calcularAreaCirculo(raio) {
+  const pi = 3.14;
+  return area = pi * (raio * raio);
+}
+console.log(calcularAreaCirculo(10));
+console.log(calcularAreaCirculo(20));
+console.log(calcularAreaCirculo(30));
